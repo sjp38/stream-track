93f362344400a8e3be513da93c72f6197f0b7da5
SeongJae Park <sjpark@amazon.de>
2020-07-28 08:10:23 +0000

    summary_outputs: Provide error message when git command fails
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- summary_outputs.py
+++ summary_outputs.py
@@ -66,7 +66,12 @@ def commit_date(hashid, repo):
     cmd = 'git --git-dir=%s/.git log %s^..%s' % (repo, hashid, hashid)
     cmd += ' --pretty="%cd" --date=format:\'%Y-%m-%d\' | head -n 1'
     date_str = subprocess.check_output(cmd, shell=True).decode().strip()
-    return datetime.datetime.strptime(date_str, '%Y-%m-%d')
+    try:
+        return datetime.datetime.strptime(date_str, '%Y-%m-%d')
+    except ValueError:
+        print('Could not get the commit date of %s' % hashid)
+        print('Please check whether \'--repo\' is properly provided.')
+        exit(1)
 
 def fmt_date_range(start, end):
     return '%s..%s' % (start.strftime('%Y-%m-%d'), end.strftime('%Y-%m-%d'))
